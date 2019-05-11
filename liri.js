6ebd8471ee6ac0bb408d64f5b7e2ac9283b3cce3
am-gforce <47460424+am-gforcehub@users.noreply.github.com>
2019-05-10 21:58:15 -0500

    added random

--- liri.js
+++ liri.js
@@ -33,7 +33,7 @@ switch (action) {
         break;
 
     case 'do-what-it-says':
-        getRandom();
+        getRandom(parameter);
 
         break;
 
@@ -52,9 +52,9 @@ function bandsInTown() {
 /******Spotify Function*****/
 
 function spotSong() {
-    console.log(" working");
+
     if (parameter === '' || parameter === null) {
-        parameter = 'The Sign'
+        parameter = 'The Sign Ace of Base'
     }
 
     var spotify = new Spotify(keys.spotify)
@@ -86,14 +86,19 @@ function movieInfo(parameter) {
 
     if (parameter === '' || parameter === null) {
         parameter = 'Mr. Nobody'
+        console.log("If you haven't watched Mr. Nobody, then you should: <http://www.imdb.com/title/tt0485947/> It's on Netflix!");
     }
-    request("http://www.omdbapi.com/?t=" + parameter + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
+    request("http://www.omdbapi.com/?t=" + parameter + "&y=&plot=short&tomatoes=true&apikey=trilogy", function (error, response, body) {
         if (!error && response.statusCode === 200) {
+
+            //couldn't get rotten tomatoes to work used 
+            //tomatoes=true and file = .tomatoRating
 
 
             console.log('--------------------');
             console.log('Movie Title: ' + JSON.parse(body).Title);
             console.log('Release Year: ' + JSON.parse(body).Year);
+            console.log('Rotten Tomato Rating: ' + JSON.parse(body).tomatoRating);
             console.log('IMDb Rating: ' + JSON.parse(body).imdbRating);
             console.log('Country: ' + JSON.parse(body).Country);
             console.log('Language: ' + JSON.parse(body).Language);
@@ -108,5 +113,14 @@ function movieInfo(parameter) {
 
 
 function getRandom() {
-    console.log("yay working");
+    //console.log("Read text function called.");
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            console.log(error);
+        } else {
+            //var dataArr = data.split(',');
+            spotSong(data[1]);
+        }
+
+    });
 }
