156de3a25b6135e91a3a03ff764ec016041bd01d
am-gforce <47460424+am-gforcehub@users.noreply.github.com>
2019-05-11 16:09:24 -0500

    added bands in town and random.txt

--- liri.js
+++ liri.js
@@ -1,11 +1,15 @@
-//code that reads and set any enviroment variables with the dotenv package
+//require .env file
 require("dotenv").config();
-
-var axios = require("axios");
+// var axios = require("axios");
+//require keys for all 
 var keys = require("./keys.js");
+//require for spotify
 var Spotify = require('node-spotify-api');
+//require moment for time in bands
 var moment = require("moment");
+//require request for omdb
 var request = require('request');
+//require file system for random
 var fs = require("fs");
 
 //process.argv arguments in the object we want to get to 2 and 3
@@ -21,7 +25,7 @@ var parameter = process.argv.slice(3).join(" ");
 switch (action) {
 
     case 'concert-this':
-        bandsInTown();
+        bandsInTown(parameter);
         break;
 
     case 'spotify-this-song':
@@ -36,19 +40,48 @@ switch (action) {
         getRandom(parameter);
 
         break;
-
-
-
 }
 
-
-
-
-//Bands in TOwn
+/********Bands in Town Function********/
 
 function bandsInTown() {
-    console.log("BandsinTown working");
-}
+
+    // if (parameter === '' || parameter === null) {
+    //     parameter = 'Ariana Grande'
+    // }
+
+    var bands = (keys.bands)
+    /*Request using bandsintown API*/
+
+    request("http://rest.bandsintown.com/artists/" + parameter + "/events?app_id=" + bands, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+
+
+            // capture obejct data and use json to console log
+            var userBand = JSON.parse(body);
+            // use loop to access data
+            if (userBand.length > 0) {
+                for (i = 0; i < 1; i++) {
+
+                    // CONSOLE DESIRED DATA USING E6 SYNTAX
+                    console.log(`\n\nArtist: ${userBand[i].lineup[0]} 
+                    \nVenue: ${userBand[i].venue.name}
+                    \nVenue Location: ${userBand[i].venue.latitude},
+                    ${userBand[i].venue.longitude}
+                    \nVenue City: ${userBand[i].venue.city}, 
+                    ${userBand[i].venue.country}`)
+
+                    // MOMENT.JS TO FORMAT THE DATE MM/DD/YYYY
+                    var concertDate = moment(userBand[i].datetime).format("MM/DD/YYYY hh:00 A");
+                    console.log(`Date and Time: ${concertDate}\n\n- - - - -`);
+                };
+            } else {
+                console.log('Band or concert not found!');
+            };
+        };
+    });
+};
+
 /******Spotify Function*****/
 
 function spotSong() {
@@ -88,17 +121,19 @@ function movieInfo(parameter) {
         parameter = 'Mr. Nobody'
         console.log("If you haven't watched Mr. Nobody, then you should: <http://www.imdb.com/title/tt0485947/> It's on Netflix!");
     }
+    /*Request using omdb API*/
+
     request("http://www.omdbapi.com/?t=" + parameter + "&y=&plot=short&tomatoes=true&apikey=trilogy", function (error, response, body) {
         if (!error && response.statusCode === 200) {
 
             //couldn't get rotten tomatoes to work used 
-            //tomatoes=true and file = .tomatoRating
+            //tomatoes=true and field in array Rating[1].Value
 
 
             console.log('--------------------');
             console.log('Movie Title: ' + JSON.parse(body).Title);
             console.log('Release Year: ' + JSON.parse(body).Year);
-            console.log('Rotten Tomato Rating: ' + JSON.parse(body).tomatoRating);
+            console.log('Rotten Tomato Rating: ' + JSON.parse(body).Ratings[1].Value);
             console.log('IMDb Rating: ' + JSON.parse(body).imdbRating);
             console.log('Country: ' + JSON.parse(body).Country);
             console.log('Language: ' + JSON.parse(body).Language);
@@ -110,16 +145,20 @@ function movieInfo(parameter) {
     });
 }
 
-
+/*****Random.txt******/
 
 function getRandom() {
-    //console.log("Read text function called.");
     fs.readFile("random.txt", "utf8", function (error, data) {
         if (error) {
             console.log(error);
+            /*catch data and use the split method to seperate objects within our new array*/
         } else {
-            //var dataArr = data.split(',');
-            spotSong(data[1]);
+            var dataArr = data.split(',');
+            /*pass in parameters set*/
+            action = (dataArr[0]);
+            parameter = dataArr[1];
+            /*call funtion*/
+            spotSong(action, parameter);
         }
 
     });
